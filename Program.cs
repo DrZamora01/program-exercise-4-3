3b79cf7bbed50af1e8c4240d62aaf7b75f68e6be
DrZamora01 <112430390+DrZamora01@users.noreply.github.com>
2022-09-05 21:32:42 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+class Admission
+{
+    static void Main()
+    {
+        Console.WriteLine("enter student gpa: ");
+        double Gpa = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("enter student test score: ");
+        int testScore = Convert.ToInt32(Console.ReadLine());
+
+
+
+        if (testScore >= 60 && Gpa >= 3.0 || testScore >= 80 && Gpa < 3.0)
+        {
+            Console.WriteLine("Accept");
+        }
+
+        //if (Gpa < 3.0 || testScore < 60)
+        // {Console.WriteLine("Accept");}
+
+
+        else { Console.WriteLine("Reject"); }
+
+
+    }
+}
